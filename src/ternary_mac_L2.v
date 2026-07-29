4ab70c9e5e2ab2b027359a12ef8a6a209c53e07c
Tri Nguyen <tri.nguyen@student.kuleuven.be>
2026-07-29 13:32:13 +0000

    macL2

--- src/ternary_mac_L2.v
+++ src/ternary_mac_L2.v
@@ -1,17 +1,17 @@
-module ternary_mac_L1(input wire clk,
+module ternary_mac_L2(input wire clk,
                       input wire rst_n,
                       input wire en,
                       input wire clear,
-                      input wire [3:0] pixel,
+                      input wire [7:0] neuron,
                       input wire signed [1:0] weight,
-                      output reg signed [8:0] accumulator);
+                      output reg signed [13:0] accumulator);
     
-    reg signed [8:0] acc_next;
+    reg signed [13:0] acc_next;
 
     always @(*) begin
         case(weight)
-          2'sd1: acc_next = accumulator + {{5{1'b0}},pixel};
-          -2'sd1: acc_next = accumulator - {{5{1'b0}},pixel};
+          2'sd1: acc_next = accumulator + {{6{1'b0}},neuron};
+          -2'sd1: acc_next = accumulator - {{6{1'b0}},neuron};
           2'sd0: acc_next = accumulator;
           default: acc_next = accumulator;
         endcase
